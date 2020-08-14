97c4f27a7fa549facd2af93bf05106e173ad5868
wei.yang <wei.yang@datatom.com>
2020-08-14 09:30:30 +0800

    feat: 添加amqp测试

--- amqpcpp/evconsume.cpp
+++ amqpcpp/evconsume.cpp
@@ -7,45 +7,69 @@ int main() {
     auto *loop = EV_DEFAULT;
     AMQP::LibEvHandler handler(loop);
 
-    AMQP::Address address("amqp://admin:datatom.com@192.168.50.97:31680");
+    AMQP::Address address("amqp://admin:datatom.com@192.168.50.97:32550");
     AMQP::TcpConnection connection(&handler, address);
 
     AMQP::TcpChannel channel(&connection);
-    
+
     //消费消息
-    auto startCb = [](const std::string &consumertag){
+    auto startCb = [](const std::string &consumertag) {
         std::cout << "consume operation started" << std::endl;
     };
-    auto errorCb = [](const char* message){
-        std::cout << "consume operation failed " << std::endl;
+    auto errorCb = [](const char *message) {
+        std::cout << "consume operation failed " << message << std::endl;
     };
-    auto messageCb = [&channel](const AMQP::Message &message, uint64_t deliveryTag, bool redelivered){
+    auto messageCb = [&channel](const AMQP::Message &message,
+                                uint64_t deliveryTag, bool redelivered) {
         std::cout << "message received: ********* " << std::endl;
         //处理消息
         cout << "exchange: " << message.exchange() << endl;
         cout << "routingkey: " << message.routingkey() << endl;
-        //string data(message.body());
-        //int size = message.bodySize();
-        //cout << "data: " << data.substr(0, size) << endl;
+        // string data(message.body());
+        // int size = message.bodySize();
+        // cout << "data: " << data.substr(0, size) << endl;
         string data1(message.body(), message.body() + message.bodySize());
         cout << "data: " << data1 << endl;
         cout << "tag: " << deliveryTag << endl;
-        //acknowledege the message
         channel.ack(deliveryTag);
+        // acknowledege the message
     };
+    //channel.consume("cpp-test1")
+    channel.consume("cpp-test1")
+    //channel.consume("heartbeat")
+        .onReceived(messageCb)
+        .onSuccess(startCb)
+        .onError(errorCb);
+    //channel.get("cpp-test1")
+    //    .onSuccess(messageCb)
+    //    .onError(errorCb);
+    ev_run(loop, 0);
+    return 0;
     //注意：onSuccess仅表示消费消息的操作开始，而onReceived才代表收到了数据
-    
-    //AMQP::Message类包含了消息的全部内容，包括真实数据，消息头，甚至exchange和routingkey。
-    
-    //deliveryTag是收到消息后的特定标识符，使用它返回响应。RabbitMq会在接收到响应后删除消息，
+
+    // AMQP::Message类包含了消息的全部内容，包括真实数据，消息头，甚至exchange和routingkey。
+
+    // deliveryTag是收到消息后的特定标识符，使用它返回响应。RabbitMq会在接收到响应后删除消息，
     //如果忘记执行 Channel:ack() 方法，RabbitMq会因为内存用尽而崩溃
-    
-    //消息的消费是持续进行的，直到使用 Channel::cancel() 停止消费。如果关闭通道或者TCP连接，消费也会停止。
+
+    //消息的消费是持续进行的，直到使用 Channel::cancel()
+    //停止消费。如果关闭通道或者TCP连接，消费也会停止。
     //可以通过QOS防止消费者被大量消息搞崩溃。QOS决定了消费者可以持有的未发送ack的数据量，RabbitMq会暂存后续的消息。
     //该属性通过 Channel::setQos() 设置
-    //channel.consume("cpp-test").onReceived(messageCb).onSuccess(startCb).onError(errorCb);
-    AMQP::Table argus;
-    channel.consume("cpp-test", argus).onReceived(messageCb).onSuccess(startCb).onError(errorCb);
+    // channel.consume("cpp-test").onReceived(messageCb).onSuccess(startCb).onError(errorCb);
+    //判读队列是否存在
+    auto successCB = [&]() {
+        channel.consume("cpp-test")
+            .onReceived(messageCb)
+            .onSuccess(startCb)
+            .onError(errorCb);
+    };
+    auto queueErrorCB = [](const char *message) {
+        cout << "队列不存在" << endl;
+    };
+    channel.declareQueue("cpp-test", AMQP::passive)
+        .onSuccess(successCB)
+        .onError(queueErrorCB);
 
     ev_run(loop, 0);
     return 0;
--- amqpcpp/evpublish.cpp
+++ amqpcpp/evpublish.cpp
@@ -3,20 +3,25 @@
 #include <amqpcpp/libev.h>
 
 using namespace std;
+void aa() { cout << "desclarExchange AA: success" << endl; }
+void bb(const char *message) {
+    cout << "declareExchange BB: " << message << endl;
+}
 int main() {
     auto *loop = EV_DEFAULT;
     AMQP::LibEvHandler handler(loop);
+    string exName = "cpp-exchange1";
+    //string quName = "cpp-test1";
+    string quName = "transcode";
 
-    AMQP::Address address("amqp://admin:datatom.com@192.168.50.97:31680");
+    AMQP::Address address("amqp://admin:datatom.com@192.168.50.97:32550");
     AMQP::TcpConnection connection(&handler, address);
 
     AMQP::TcpChannel channel(&connection);
 
-    channel.declareExchange("cpp-exchange", AMQP::fanout)
-        .onSuccess([]() { cout << "desclarExchange: success" << endl; })
-        .onError([](const char *message) {
-             cout << "declareExchange: " << message << endl;
-         });
+    auto succss = []() { cout << "desclarExchange: success" << endl; };
+    channel.declareExchange(exName, AMQP::fanout).onSuccess(aa).onError(
+        bb);
 
     //设置队列的属性
     // AMQP::durable,持久化，如果启用，队列将会在server重启前后都有效
@@ -24,36 +29,40 @@ int main() {
     // AMQP::passive,惰性，如果没有声明队列，那么在执行到使用的时候会导致异常，多用于检查队列是否存在
     // AMQP::exclusive，排他性，他的队列仅针对该连接而存在，并且在连接断开后会自动删除
     AMQP::Table args;
-    args.set("x-max-priority", 10);// 设置队列的优先级
-    //args.set("x-max-length", 5); //设置队列的最大长度
-    //args.set("x-max-length-bytes",100);//设置队列的最大自己数目
-    
-    channel.declareQueue("cpp-test", AMQP::durable, args)
+    args.set("x-max-priority", 10);  // 设置队列的优先级
+    // args.set("x-max-length", 5); //设置队列的最大长度
+    // args.set("x-max-length-bytes",100);//设置队列的最大自己数目
+
+    channel.declareQueue(quName, AMQP::durable, args)
         .onSuccess([&connection](const std::string &name, uint32_t messagecount,
                                  uint32_t consumercount) {
              std::cout << "decalared queue: " << name << std::endl;
              connection.close();
          })
-        .onError([](const char *message) {
-             cout << "decalared queue failed: " << message << endl;
-         });
+        .onError(bb);
     //绑定队列
-    channel.bindQueue("cpp-exchange", "cpp-test", "my-routing-key")
+    channel.bindQueue(exName, quName, "my-routing-key")
         .onSuccess([]() { cout << "bindQueue: success" << endl; })
         .onError([](const char *message) {
              cout << "bindQueue Failed: " << message << endl;
          });
     //发送消息
-    channel.publish("cpp-exchange", "my-routing-key",  "first message");
-    channel.publish("cpp-exchange", "my-routing-key",  "second message");
-    channel.publish("cpp-exchange", "my-routing-key",  "three message");
-    channel.publish("cpp-exchange", "my-routing-key",  "four message");
+    //string aa = "{\"taskId\":\"111\",\"taskDetail\":{\"srcpath\":\"\/root\/video1\/video-H263-AC3.avi\",\"destpath\":\"test.mp4\",\"videocodec\":\"H264\",\"audiocodec\":\"AAC\"}}";
+    string aa = "{\"taskId\":\"111\",\"taskDetail\":{\"srcpath\":\"wei:video-H263-AC3.avi\",\"srchost\":\"192.168.50.186:7480\",\"srcaksk\":\"791f66218f5a090391b38ef501811ffc,18c7cf6f142b9f0e194183dac6d15926\",\"destpath\":\"wei:transcode.mp4\",\"desthost\":\"192.168.50.186:7480\",\"destaksk\":\"791f66218f5a090391b38ef501811ffc,18c7cf6f142b9f0e194183dac6d15926\",\"videocodec\":\"H264\",\"audiocodec\":\"AAC\"}}";
+    string bb = "{\"taskId\":\"222\",\"taskDetail\":{\"name1\":\"weiyang\"}}";
+    string dd = "{\"taskId\":\"333\",\"taskDetail\":{\"srcpath\":\"wei:video-H263-AC3.avi\",\"srchost\":\"192.168.50.186:7480\",\"srcaksk\":\"791f66218f5a090391b38ef501811ffc,18c7cf6f142b9f0e194183dac6d15926\",\"destpath\":\"wei:interce.avi\",\"desthost\":\"192.168.50.186:7480\",\"destaksk\":\"791f66218f5a090391b38ef501811ffc,18c7cf6f142b9f0e194183dac6d15926\",\"start\":\"30\",\"end\":\"50\"}}";
+    string cc = "{\"taskId\":\"444\",\"taskDetail1\":{\"name1\":\"weiyang\"}}";
+    string task1 = "{\"taskId\":\"555\",\"taskDetail\":{\"srcpath\":\"wei:video-H263-AC3.avi\",\"srchost\":\"192.168.50.186:7480\",\"srcaksk\":\"791f66218f5a090391b38ef501811ffc,18c7cf6f142b9f0e194183dac6d15926\",\"destpath\":\"wei:transcode1.mp4\",\"desthost\":\"192.168.50.186:7480\",\"destaksk\":\"791f66218f5a090391b38ef501811ffc,18c7cf6f142b9f0e194183dac6d15926\",\"videocodec\":\"H264\",\"audiocodec\":\"AAC\"}}";
+    channel.publish(exName, "my-routing-key", aa);
+    channel.publish(exName, "my-routing-key", dd);
+    channel.publish(exName, "my-routing-key", bb);
+    channel.publish(exName, "my-routing-key", cc);
+    channel.publish(exName, "my-routing-key", task1);
     //设置消息的优先级
-    string name("five message");
-    AMQP::Envelope enve(name.c_str(), name.length());
-    enve.setPriority(10);
-    channel.publish("cpp-exchange", "my-routing-key",  enve);
-    channel.publish("cpp-exchange", "my-routing-key",  "six message");
+    //string name("five message");
+    //AMQP::Envelope enve(name.c_str(), name.length());
+    //enve.setPriority(10);
+    //channel.publish("cpp-exchange", "my-routing-key", enve);
 
     ev_run(loop, 0);
     return 0;
